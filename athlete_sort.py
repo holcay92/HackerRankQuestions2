60d0143805cd4a62bf6a7fd3b7f5c4d00cd40bdf
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-03-22 01:39:09 +0300

    Solution added

--- athlete_sort.py
+++ athlete_sort.py
@@ -1,7 +1,6 @@
 #h.halil olcay
-import numpy as np
 
-N_M:int = list(map(int, input().split()))
+N_M: int = list(map(int, input().split()))
 if not N_M[0] >= 1: exit("constrain for N")
 if not N_M[1] <= 1000: exit("constrain for M")
 
@@ -12,6 +11,9 @@ for i in range(0, (N_M[0])):
 #print(attributes)
 K = int(input())
 if not 0 <= K < N_M[1]: exit("constrain for K")
-attributes = np.sort(attributes, axis=K)
-print(attributes)
+
+attributes = sorted(attributes, key=lambda x: x[K])
+
+for i in range(0, int(N_M[0])):
+    print(*attributes[i], sep=" ")
 
